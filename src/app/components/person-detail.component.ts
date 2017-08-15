fbe879f7ab15d43f4defa19797b3c723cfb7fa10
Geektimus <alexander.kno@gmail.com>
2017-08-15 00:10:44 -0500

    Replace form group with form builder for readability, Add basic validators

--- src/app/components/person-detail.component.ts
+++ src/app/components/person-detail.component.ts
@@ -1,14 +1,22 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
     selector: 'app-person-detail',
     templateUrl: './person-detail.component.html'
 })
 export class PersonDetailComponent {
-    personForm = new FormGroup({
-        name: new FormControl(),
-        mail: new FormControl(),
-        phone: new FormControl()
-    });
+    personForm: FormGroup;
+
+    constructor(private fb: FormBuilder) {
+        this.createForm();
+    }
+
+    createForm() {
+        this.personForm = this.fb.group({
+            name: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(20)]],
+            mail: ['', [Validators.required, Validators.email]],
+            phone: ''
+        });
+    }
 }

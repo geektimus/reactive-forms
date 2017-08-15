2e0881dc83755fe00f7d6370ae12bd6673e4210d
Geektimus <alexander.kno@gmail.com>
2017-08-15 14:38:14 -0500

    Fix number validator as it didn't require the function return type

--- src/app/shared/custom-validators.ts
+++ src/app/shared/custom-validators.ts
@@ -2,15 +2,12 @@ import { AbstractControl, ValidatorFn, FormGroup } from '@angular/forms';
 
 
 export class CustomValidators {
-    static number(): ValidatorFn {
-        return (control: AbstractControl): { [key: string]: any } => {
-            const regexp = /^\d+$/;
-            const value = control.value;
-            console.log('Checking the value of: ', value);
-            if (value === '') {
-                return null;
-            }
-            return !regexp.test(value) ? { 'invalidNumber': { value: control.value } } : null;
-        };
+    static number(control: AbstractControl) {
+        const regexp = /^\d+$/;
+        const value = control.value;
+        if (value === '') {
+            return null;
+        }
+        return !regexp.test(value) ? { 'invalidNumber': { value: control.value } } : null;
     }
 }
